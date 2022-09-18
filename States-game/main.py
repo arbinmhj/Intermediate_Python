82a253b4cb9e373c257d543ee77e98bc73e8591a
Arbin Maharjan <49609055+arbinmhj@users.noreply.github.com>
2022-09-18 15:13:09 +0545

    Create main.py

---
+++ States-game/main.py
@@ -0,0 +1,42 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_state = []
+
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 States Correct",
+                                    prompt="What's another state's name:").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_state:
+                missing_states.append(state)
+        df = pd.DataFrame(missing_states)
+        df.to_csv("remaining_states.csv")
+        break
+    if answer_state in all_states:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+
+
+# TO GET THE COORDINATES FROM THE SCREEN
+# def get_mouse_click_cor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_cor)
